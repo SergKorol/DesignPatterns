01dedef9dceddc468d21719ed1481cf06f0c882c
Serhii Korol <korol_sergey@me.com>
2025-10-31 11:48:22 +0200

    fixed inconsistent spacing in default name assignment within FileSystemElement constructor

--- VisitorPattern/Ordinary/FileSystemElement.cs
+++ VisitorPattern/Ordinary/FileSystemElement.cs
@@ -2,7 +2,7 @@ namespace VisitorPattern.Ordinary;
 
 public abstract class FileSystemElement(string name)
 {
-    protected string Name { get; } = string.IsNullOrEmpty(name) ?  "root" : name;
+    protected string Name { get; } = string.IsNullOrEmpty(name) ? "root" : name;
 
     public abstract long GetSize();
     public abstract void PrintReport(int depth = 0);
